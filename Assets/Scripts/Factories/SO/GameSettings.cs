752bd492e8cd70021e8d8cd32fe8a0277861aa4a
d.sharov <overlord.shor@gmail.com>
2023-06-13 23:39:14 +0300

    feat: settings so

--- Assets/Scripts/Factories/SO/GameSettings.cs
+++ Assets/Scripts/Factories/SO/GameSettings.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 namespace ProductionGame.SO
 {
-    [CreateAssetMenu(fileName = nameof(GameSettings), menuName = "ProductionGame/Building Settings", order = 1)]
+    [CreateAssetMenu(fileName = nameof(GameSettings), menuName = "ProductionGame/Game Settings", order = 1)]
     public class GameSettings : ScriptableObject
     {
         [SerializeField] private BuildingSettings _marketBuildingSettings;
